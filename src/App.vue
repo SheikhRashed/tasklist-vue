2e4d8ed85e609430b4f38e8723935c13562e403d
SheikhRashed <devrashed06@gmail.com>
2022-01-12 07:56:44 +0600

    update: removed postcss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template lang="">
 	<div>
-		<h2>hello</h2>
+		<header>
+			<h1>Vue Course Goals</h1>
+		</header>
+		<section id="user-goals">
+			<h2>My course goals</h2>
+			<input type="text" />
+			<button>Add Goal</button>
+			<p>No goals have been added yet - please start adding some!</p>
+			<ul>
+				<li>Goal</li>
+			</ul>
+		</section>
 	</div>
 </template>
 <script>
 	export default {
 		name: 'App',
 		data() {
-			return {}
+			return {
+				goals: [],
+			}
 		},
 	}
 </script>
